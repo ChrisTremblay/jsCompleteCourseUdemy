19457572ca2f9684b1207e1c3d9c771c09305591
Chris <tremblay.christo@gmail.com>
2018-10-05 16:57:57 +0200

    Objects

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -30,14 +30,66 @@ console.log(name);*/
    Lecture 10 */
 /////////////////////////////
 
-const currentYear = 2018;
-const yearJohn = 1966;
-const yearMark = 1967;
+/*const johnTeamPoints = [90, 120, 123];
+const mikeTeamPoints = [116, 120, 120];
+const maryTeamPoints = [50, 134, 200];
 
-const ageJohn = currentYear - yearJohn;
-const ageMark = currentYear - yearMark;
+var avgJohn = 0, avgMike = 0, avgMary = 0;
 
-console.log(ageJohn);
-console.log(ageMark);
+for(var i = 0; i<johnTeamPoints.length; i++){
+	avgJohn += johnTeamPoints[i];
+	avgMike += mikeTeamPoints[i];
+	avgMary += maryTeamPoints[i]; 
+}
 
-ageJohn > ageMark ? console.log("John older") : console.log("Mark older");
+avgJohn = Math.round(avgJohn / johnTeamPoints.length);
+avgMike = Math.round(avgMike / mikeTeamPoints.length);
+avgMary = Math.round(avgMary / maryTeamPoints.length);
+
+console.log(`${avgJohn}, ${avgMike}, ${avgMary}`)
+
+if(avgJohn > avgMike && avgJohn>avgMary){
+	console.log(`John's team wins with an average of ${avgJohn} points.`);
+}else if(avgMike > avgJohn && avgMike>avgMary){
+	console.log(`Mike's team wins with an average of ${avgMike} points.`);
+}else if(avgMary > avgJohn && avgMary>avgMike){
+	console.log(`Mary's team wins with an average of ${avgMary} points.`);
+}*/
+
+/////////////////////////////
+/* Functions
+   Lecture 20 */
+/////////////////////////////
+
+/*function calculateAge(birthYear){
+	return 2018 - birthYear;
+}
+
+console.log(calculateAge(1992));
+
+function yearsToRetirement(birthYear, firstName){
+	(62 - calculateAge(birthYear) > 0) ? console.log(`${firstName} you will retire in ${62 - calculateAge(birthYear)} years`) : console.log(`${firstName} enjoy your retirement`);
+}
+
+yearsToRetirement(1992, "Chris");*/
+
+/////////////////////////////
+/* Arrays
+   Lecture 22 */
+/////////////////////////////
+
+const bills = [124, 48, 268];
+const tips = [0.2, 0.15, 0.1];
+
+function calculateTip(bills){
+	toPay = [];
+	for(var i = 0; i<bills.length; i++){
+		if(bills[i]<50){toPay.push(bills[i]+bills[i]*tips[0]);}
+		else if(bills[i]>=50 && bills[i] <= 200){toPay.push(bills[i]+bills[i]*tips[1]);}
+		else{toPay.push(bills[i]+bills[i]*tips[2]);}
+	}
+	return toPay;
+}
+
+var totalToPay = calculateTip(bills);
+console.log(totalToPay);
