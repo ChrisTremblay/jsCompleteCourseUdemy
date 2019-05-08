cd2a79118f50caa5113111c1f74cf3ac837cf0cd
Chris <tremblay.christo@gmail.com>
2019-05-08 16:52:05 +0200

    Update app.js
    Added clearing input fields

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -91,8 +91,19 @@ var UIController = (function(){
         	newHtml = newHtml.replace('%value%', obj.value);
 
         	document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+     	},
 
+     	clearFields: function(){
+     		var fields, fieldsArr;
 
+     		fields = document.querySelectorAll(DOMstrings.inputDescription + ', '+ DOMstrings.inputValue);
+     		fieldsArr = Array.prototype.slice.call(fields);
+
+     		fieldsArr.forEach(function(current, index, array){
+     			current.value = "";
+     		});
+
+     		fieldsArr[0].focus();
      	},
 
 		getDOMstrings: function(){
@@ -128,6 +139,8 @@ var controller = (function(budgetCtrl, UICtrl){
 
 		// 3. Add the new item to the UI
 		UICtrl.addListItem(newItem, input.type);
+		UICtrl.clearFields();
+
 		// 4. Calculate the budget
 
 		// 5. Display the budget on the UI
